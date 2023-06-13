df7ac9810e07c2bef756db36758805725c710f51
CoderKeung <88510062+CoderKeung@users.noreply.github.com>
2023-06-13 10:53:55 +0800

    添加弹窗

--- static/script.js
+++ static/script.js
@@ -1,8 +1,12 @@
 $("#update").click(()=>{
+    $("#update").find("span").text("正在更新")
     $.get(
         "/update",
         (data)=>{
             console.log(data)
+            swal("成功", "更新完成！", "success").then(()=>{
+                location.reload();
+            });
         }
     )
 })
